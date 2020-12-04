526e1241ec42b3b84f84e477883f11f16bb466fd
David J. Sanderson <sanderdj90@gmail.com>
2020-12-04 11:39:01 -0700

    refactor part one

--- 2020/day-4.js
+++ 2020/day-4.js
@@ -4,35 +4,26 @@ const _ = require("lodash");
 const text = fs.readFileSync("./day-4.txt").toString("utf-8");
 const lines = text.split("\n");
 
+// PART 1
+optionalFields = ["cid"];
+requiredFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
 
-let passports = [];
 currentPassport = "";
-
+count = 0 
+let validatePassport = passport => {
+  let fields = passport.split(' ')
+  .map((el) => el.split(":")[0])
+  .filter((fieldName) => requiredFields.indexOf(fieldName) !== -1);
+  let isValid = fields.length >= requiredFields.length;
+  return isValid;
+}
 lines.forEach((line) => {
   let endOfPassport = line === "";
   if (endOfPassport) {
-    passports.push(currentPassport);
+    validatePassport(currentPassport) ? count++ : null;
     currentPassport = "";
   } else {
     currentPassport += ` ${line}`;
   }
 });
-
-optionalFields = ["cid"];
-requiredFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
-
-const validate = (passports) => {
-  let validPassports = passports.filter((el) => {
-    let passport = el.split(" ");
-    let fields = passport
-      .map((el) => el.split(":")[0])
-      .filter((fieldName) => requiredFields.indexOf(fieldName) !== -1);
-      let isValid = fields.length >= requiredFields.length;
-      return isValid;
-    });
-    return validPassports
-};
-
-
-let result = validate(passports)
-console.log(result.length)
+console.log(count)
