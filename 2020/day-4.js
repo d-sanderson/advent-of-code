3522e06ae545e47ea670ac4ecec15a057063b4df
David J. Sanderson <sanderdj90@gmail.com>
2020-12-04 10:46:41 -0700

    day-4 part one

--- 2020/day-4.js
+++ 2020/day-4.js
@@ -1,16 +1,37 @@
 const fs = require("fs");
-const _ = require("lodash")
-const text = fs.readFileSync("./day-4.txt").toString('utf-8');
-const passports = text.split("\n")
+const { keys } = require("lodash");
+const _ = require("lodash");
+const text = fs.readFileSync("./day-4.txt").toString("utf-8");
+const lines = text.split("\n");
 
-optionalFields = ['cid']
-requiredFields = [ 'byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'].sort()
 
-let t = passports.filter(el => {
-    console.log(el)
-    let passport = el.split(' ');
-    let fields = passport.map(el => el.split(':')[0]).filter(fieldName => requiredFields.indexOf(fieldName) !== -1)
-    let isValid = fields.length >= requiredFields.length 
-    return isValid
-})
-console.log(t.length)
+let passports = [];
+currentPassport = "";
+
+lines.forEach((line) => {
+  let endOfPassport = line === "";
+  if (endOfPassport) {
+    passports.push(currentPassport);
+    currentPassport = "";
+  } else {
+    currentPassport += ` ${line}`;
+  }
+});
+
+optionalFields = ["cid"];
+requiredFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+
+const validate = (passports) => {
+  let validPassports = passports.filter((el) => {
+    let passport = el.split(" ");
+    let fields = passport
+      .map((el) => el.split(":")[0])
+      .filter((fieldName) => requiredFields.indexOf(fieldName) !== -1);
+      let isValid = fields.length >= requiredFields.length;
+      return isValid;
+    });
+    return validPassports
+};
+
+
+let result = validate(passports)
--- index.js
+++ index.js
@@ -2,10 +2,10 @@ let art = `
         .
        ,O,
       ,OOO,                 a           dddddd    vvvvvv       vvvvvv
-'oooooOOOOOooooo'          aaa          dd    dd   vvvv        vvvv
-  'OOOOOOOOOOO'           aaaaa         dd     dd   vvv        vvv
-    'OOOOOOO'            aa   aa        dd     dd    vv        vv
-    OOOO'OOOO          aaaaaaaaaaa      dd     dd     vvvv  vvvv
+'oooooOOOOOooooo'          aaa          dd    ddd   vvvv        vvvv
+  'OOOOOOOOOOO'           aaaaa         dd     ddd   vvv        vvv
+    'OOOOOOO'            aa   aa        dd     dddd    vv        vv
+    OOOO'OOOO          aaaaaaaaaaa      dd     ddd     vvvv  vvvv
    OOO'   'OOO        aaaa     aaaa     dd    ddd      vvv  vvv
   O'         'O      aaaaa     aaaaa    ddddddd          vvvv
 
