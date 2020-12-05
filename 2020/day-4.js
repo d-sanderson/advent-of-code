e7df750cf274224a198d64314b17ec9a0786e299
David J. Sanderson <sanderdj90@gmail.com>
2020-12-04 22:41:14 -0700

    solution one day 4

--- 2020/day-4.js
+++ 2020/day-4.js
@@ -9,14 +9,15 @@ optionalFields = ["cid"];
 requiredFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
 
 currentPassport = "";
-count = 0 
-let validatePassport = passport => {
-  let fields = passport.split(' ')
-  .map((el) => el.split(":")[0])
-  .filter((fieldName) => requiredFields.indexOf(fieldName) !== -1);
+count = 0;
+let validatePassport = (passport) => {
+  let fields = passport
+    .split(" ")
+    .map((el) => el.split(":")[0])
+    .filter((fieldName) => requiredFields.indexOf(fieldName) !== -1);
   let isValid = fields.length >= requiredFields.length;
   return isValid;
-}
+};
 lines.forEach((line) => {
   let endOfPassport = line === "";
   if (endOfPassport) {
@@ -26,4 +27,24 @@ lines.forEach((line) => {
     currentPassport += ` ${line}`;
   }
 });
-console.log(count)
+console.log(count);
+
+// Part Two
+const isValidBirthYear = (byr) => 1920 <= byr && byr <= 2002;
+const isValidIssueYear = (year) => 2010 <= year && year <= 2020;
+const isValidExpYear = (eyr) => 2020 <= eyr && eyr <= 2030;
+const isValidHeight = (hgt) => {
+let regex =
+};
+const isValidHairColor = (hcl) => {
+  regex = /(#[a-f|0-9]{6})/
+  return regex.test(hcl)
+};
+const isValidHairColor = (hcl) => hcl;
+const isValidEyeColor = (ecl) => {
+  const validEyeColors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+  validEyeColors.includes(ecl);
+};
+const isValidPID = (pid) => !isNaN(pid)&& pid.length === 9;
+
+
