8b818310e027371088e1d1181865a8986cef08c7
David <sanderdj90@gmail.com>
2020-12-05 20:22:02 -0700

    refact organize day-4

--- 2020/day-4.js
+++ 2020/day-4.js
@@ -4,30 +4,64 @@ const _ = require("lodash");
 const text = fs.readFileSync("./day-4.txt").toString("utf-8");
 const lines = text.split("\n");
 
+// PART 1
+optionalFields = ["cid"];
+requiredFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
+
+currentPassport = "";
+count = 0;
+let validatePassport = (passport) => {
+  let fields = passport
+    .split(" ")
+    .map((el) => evaluateField(el))
+    .filter((el) => el == true);
+  let isValid = fields.length === requiredFields.length;
+  return isValid;
+};
+
+lines.forEach((line) => {
+  let endOfPassport = line === "\r";
+  if (endOfPassport) {
+    validatePassport(currentPassport) ? count++ : null;
+    currentPassport = "";
+  } else {
+    currentPassport += ` ${line}`;
+  }
+});
+
+
 // Part Two
-const isValidBirthYear = (byr) => 1920 <= byr && byr <= 2002;
-const isValidIssueYear = (year) => 2010 <= year && year <= 2020;
-const isValidExpYear = (eyr) => 2020 <= eyr && eyr <= 2030;
-const isValidHeight = (hgt) => {
+function isValidBirthYear(byr) {
+  return 1920 <= byr && byr <= 2002;
+}
+function isValidIssueYear(year) {
+  return 2010 <= year && year <= 2020;
+}
+function isValidExpYear(eyr) {
+  return 2020 <= eyr && eyr <= 2030;
+}
+function isValidHeight(hgt) {
   if (hgt.includes("in")) {
     return /\b((5[9]|6[0-9]|7[0-6])in)\b/.test(hgt);
   }
   if (hgt.includes("cm")) {
     return /\b(1[5-8][0-9]cm|19[0-3]cm)\b/.test(hgt);
   }
-};
-const isValidHairColor = (hcl) => {
+}
+function isValidHairColor(hcl) {
   regex = /(#[a-f|0-9]{6})/;
   return regex.test(hcl);
-};
-const isValidEyeColor = (ecl) => {
+}
+function isValidEyeColor(ecl) {
   const validEyeColors = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
   return validEyeColors.includes(ecl);
-};
-const isValidPID = (pid) => !isNaN(pid) && pid.length === 9;
+}
+function isValidPID(pid) {
+  return !isNaN(pid) && pid.length === 9;
+}
 
 function evaluateField(field) {
-  field = field.replace("\r", "")
+  field = field.replace("\r", "");
   const [key, value] = field.split(":");
   switch (key) {
     case "byr":
@@ -44,32 +78,7 @@ function evaluateField(field) {
       return isValidEyeColor(value);
     case "pid":
       return isValidPID(value);
-    default:
-      return "ignore";
   }
 }
 
-// PART 1
-optionalFields = ["cid"];
-requiredFields = ["byr", "iyr", "eyr", "hgt", "hcl", "ecl", "pid"];
-
-currentPassport = "";
-count = 0;
-let validatePassport = (passport) => {
-  let fields = passport
-    .split(" ")
-    .map((el) => evaluateField(el))
-    .filter((el) => el == true);
-  let isValid = fields.length === requiredFields.length;
-  return isValid;
-};
-lines.forEach((line) => {
-  let endOfPassport = line === "\r";
-  if (endOfPassport) {
-    validatePassport(currentPassport) ? count++ : null;
-    currentPassport = "";
-  } else {
-    currentPassport += ` ${line}`;
-  }
-});
 console.log(count);
