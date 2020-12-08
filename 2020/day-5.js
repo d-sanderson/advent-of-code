2e86ca84033b20be06ebcca8dfe4462f2bb8508f
David J. Sanderson <sanderdj90@gmail.com>
2020-12-08 16:22:46 -0700

    day 5

--- 2020/day-5.js
+++ 2020/day-5.js
@@ -1,17 +1,37 @@
+const { Console } = require("console");
 const fs = require("fs");
 const _ = require("lodash");
-const text = fs.readFileSync("./day-5.txt").toString("utf-8");
-const lines = text.split("\n");
-lines.forEach(line => console.log(line.length))
+const input = fs.readFileSync("./day-5.txt").toString("utf-8");
 
-let min = 0, max = 127
-let range = [min, max]
+function findSeatId(line) {
+  const binary = line.replace(/[FL]/g, "0").replace(/[BR]/g, "1");
+  if (binary.match(/([01]{7})([01]{3})/)) {
+    const [m, row, col] = binary.match(/([01]{7})([01]{3})/);
+    const rowNum = Number(`0b${row}`);
+    const colNum = Number(`0b${col}`);
+    return rowNum * 8 + colNum;
+  }
+}
 
-const getNewRange = (char) => {
-if(char === "F") {
-    console.log(Math.floor(range[1]/2))
+let lines = input.split("\n");
+
+let highestSeatId = 0;
+let seatIds = [];
+for (const line of lines) {
+  const seatId = findSeatId(line);
+  seatIds.push(seatId);
+  if (seatId > highestSeatId) {
+    highestSeatId = seatId;
+  }
 }
-if(char === "B") {
-    
-}
+// console.log("Highest seat ID:", highestSeatId);
+
+// PART 2
+seatIds = seatIds.sort((a, b) => a - b);
+
+for (let i = 0; i < seatIds.length; i++) {
+  if (i > 0 && seatIds[i] != seatIds[i - 1] + 1) {
+    console.log("Your seat ID:", seatIds[i - 1]);
+    break;
+  }
 }
