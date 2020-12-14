ae1d9aaccf2beeb5539a1cf8237d1d5a7cbd7b0e
David <sanderdj90@gmail.com>
2020-12-13 23:59:12 -0700

    day 9 pt 1

--- 2020/day-9.js
+++ 2020/day-9.js
@@ -4,10 +4,21 @@ const _ = require("lodash");
 const text = fs.readFileSync("./day-9.txt").toString("utf-8");
 const lines = text.split("\n");
 
-let i = 26;
-let counter = 0;
-while(i < lines.length) {
-    let last25 = lines.slice(counter, counter + 25)
-    i++;
-    counter++
+let i = 25;
+while (i < lines.length - 1) {
+  let last25 = lines
+    .slice(i - 25, i)
+    .filter((el) => parseInt(el) < parseInt(lines[i]));
+  let arr = [];
+  for (let j = 0; j < last25.length; j++) {
+    for (let k = 0; k < last25.length; k++) {
+      if (parseInt(last25[j]) + parseInt(last25[k]) === parseInt(lines[i])) {
+        arr.push(parseInt(lines[i]));
+      }
+    }
+  }
+  if (!arr.includes(parseInt(lines[i]))) {
+    console.log(lines[i]);
+  }
+  i++;
 }
