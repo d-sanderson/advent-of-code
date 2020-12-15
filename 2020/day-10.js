4b7c4ed032d1cbd3f94ead751cc4575e9f555cc5
David J. Sanderson <sanderdj90@gmail.com>
2020-12-14 18:38:06 -0700

    begin day 10

---
+++ 2020/day-10.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const { last, first } = require("lodash");
+const _ = require("lodash");
+const text = fs.readFileSync("./day-10.txt").toString("utf-8");
+const lines = text.split("\n").sort((a, b) => a - b);
+
+let diff1 = 0;
+let diff3 = 0;
+for (let i = 0; i < lines.length; i++) {
+    if (i > 0 && parseInt(lines[i]) - parseInt(lines[i - 1]) === 3) {
+      diff3++;
+    }
+  else if (i > 0 && parseInt(lines[i]) - parseInt(lines[i] - 1) === 1) {
+    diff1++;
+  }
+}
+console.log((diff1) + (diff3), lines.length)
