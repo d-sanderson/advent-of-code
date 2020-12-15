d59d5e1648eaa14ad3ffd8ae792a6c0364bb831d
David <sanderdj90@gmail.com>
2020-12-14 21:16:53 -0700

    begin day 11

---
+++ 2020/day-11.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+const { last, first } = require("lodash");
+const _ = require("lodash");
+const text = fs.readFileSync("./day-11.txt").toString("utf-8");
+const lines = text.split("\n").sort((a, b) => a - b);
+
+for (let i = 0; i < 2; i++) {
+  console.log(lines[i]);
+
+  lines[i].split("")
+  .forEach((seatOrFloor, idx) => {
+    if (seatOrFloor === "L") {
+      let t, r, l, b, dtl, dtr, dbl, dbr;
+      if (i > 0) {
+        t = lines[i - 1][idx];
+        dtl = lines[i - 1][idx - 1];
+        dtr = lines[i - 1][idx + 1];
+        r = lines[i][idx + 1];
+        l = lines[i][idx - 1];
+        b = lines[i + 1][idx];
+        dbl = lines[i + 1][idx - 1];
+        dbr = lines[i + 1][idx + 1];
+
+        console.log(seatOrFloor, t);
+      }
+    }
+  });
+}
