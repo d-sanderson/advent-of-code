082e3279a5b49aeda519dce02c69ba72dc89a865
David <sanderdj90@gmail.com>
2020-12-14 20:56:37 -0700

    pt one

--- 2020/day-10.js
+++ 2020/day-10.js
@@ -4,14 +4,15 @@ const _ = require("lodash");
 const text = fs.readFileSync("./day-10.txt").toString("utf-8");
 const lines = text.split("\n").sort((a, b) => a - b);
 
-let diff1 = 0;
-let diff3 = 0;
+let diff1 = 1;
+let diff3 = 1;
 for (let i = 0; i < lines.length; i++) {
-    if (i > 0 && parseInt(lines[i]) - parseInt(lines[i - 1]) === 3) {
-      diff3++;
-    }
+  console.log(parseInt(lines[i]) - parseInt(lines[i - 1]) == 2)
+  if (i > 0 && parseInt(lines[i]) - parseInt(lines[i - 1]) === 3) {
+    diff3++;
+  }
   else if (i > 0 && parseInt(lines[i]) - parseInt(lines[i] - 1) === 1) {
     diff1++;
   }
 }
-console.log((diff1) + (diff3), lines.length)
+console.log(diff1 * diff3);
