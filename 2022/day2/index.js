d8bef2283a7f5ad8262ad1fc7cc3b14afe65df53
David <sanderdj90@gmail.com>
2023-11-15 16:29:07 -0700

    day 2

--- 2022/day2/index.js
+++ 2022/day2/index.js
@@ -1,50 +1,103 @@
 const fs = require("fs");
 const input = fs.readFileSync("./input.txt").toString("utf-8");
-const test = input.split("\r\n").map(el => [...el.replace(' ', '')]);
+const cleanedInput = input
+  .split("\r\n")
+  .map((el) => [...el.replace(" ", "")])
+  .filter((el) => !!el);
 
-let sum = 0
+let sum = 0;
+const validSelections = ["rock", "paper", "scissors"];
+const lookup = ["rock", "paper", "scissors"];
+const them = ["A", "B", "C"];
+const you = ["X", "Y", "Z"];
+
+const getChoice = (outcome, opChoice) => {
+  if(outcome === "draw") {
+    return opChoice
+  }
+  if(outcome === "win") {
+    switch(opChoice) {
+      case "paper" :
+        return "scissors"
+      case "scissors":
+        return "rock"
+      case "rock":
+        return "paper"
+    }
+  }
+  if(outcome === "lose") {
+    switch(opChoice) {
+      case "paper" :
+        return "rock"
+      case "scissors":
+        return "paper"
+      case "rock":
+        return "scissors"
+    }
+  }
+}
+const updateScore = (outcome, player) => {
+  let points = 1;
+  switch (outcome) {
+    case "draw":
+      points = 4 + validSelections.indexOf(player);
+      break;
+    case "p1":
+      points = 1 + validSelections.indexOf(player);
+      break;
+    case "p2":
+    default:
+      points = 7 + validSelections.indexOf(player);
+      break;
+  }
+  return points;
+};
 
 function rockPaperScissors(playerOne, playerTwo) {
-  const validSelections = ["lol", "rock", "paper", "scissors"];
-  // Check if the selections are valid
   if (
     !validSelections.includes(playerOne) ||
     !validSelections.includes(playerTwo)
   ) {
-    return "Invalid selection. Please choose from rock, paper, or scissors.";
+    return
   }
 
-  // Check for a tie
   if (playerOne === playerTwo) {
-    sum += 3 + validSelections.indexOf(playerTwo)
-    return "It's a tie!";
+    sum += updateScore('draw', playerTwo);
+    return
   }
 
-  // Determine the winner
-  if (
+  const winner =
     (playerOne === "rock" && playerTwo === "scissors") ||
     (playerOne === "scissors" && playerTwo === "paper") ||
     (playerOne === "paper" && playerTwo === "rock")
-  ) {
-    sum += validSelections.indexOf(playerTwo)
-    return "Player One wins!";
-  } else {
-    sum += 6
-    sum += validSelections.indexOf(playerTwo);
-    return "Player Two wins!";
-  }
+      ? "Player One"
+      : "Player Two";
+
+  const points =
+    winner === "Player One"
+      ? updateScore("p1", playerTwo)
+      : updateScore("p2", playerTwo);
+
+  sum += points;
 }
 
+// cleanedInput.forEach((t) => {
+//   const opChoice = lookup[them.indexOf(t[0])];
+//   const yourChoice = lookup[you.indexOf(t[1])];
+//   rockPaperScissors(opChoice, yourChoice);
+// });
+// console.log("Part 1", sum);
 
-const lookup = ['rock', 'paper','scissors']
 
-const them = ['A', 'B', 'C']
-const you = ['X', 'Y', 'Z']
-// console.log(test)
-test.forEach((t) => {
+// Part 2
+const outcomes = ["lose","draw", "win"]
+
+cleanedInput.forEach((t) => {
+  const outcome = outcomes[you.indexOf(t[1])]
   const opChoice = lookup[them.indexOf(t[0])];
-  const yourChoice = lookup[you.indexOf(t[1])];
-  console.log(rockPaperScissors(opChoice, yourChoice))
+  const yourChoice = getChoice(outcome, opChoice)
+  rockPaperScissors(opChoice, yourChoice)
+  console.log({outcome, opChoice, yourChoice})
 })
 
-console.log(sum)
+console.log("Part 2", sum);
